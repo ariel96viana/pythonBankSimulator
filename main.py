96cfe66c0f4d80613741b6f17d5480f024a40df4
ariel96viana <ariel96viana@gmail.com>
2022-09-26 00:45:03 -0300

    Realizando atualizacao e removendo erro.

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from funcoes_banco import FuncoesBanco
 
 operacao = 0
 saldo_total = 0
-saldo_diario = 3
+saque_diario = 3
 extrato = ""
+
 funcionamento = FuncoesBanco
 
 print(f"""
@@ -19,18 +20,15 @@ while operacao != 4:
     operacao = int(input("Digite a operação desejada: "))
 
     if operacao == 1:
-
-        funcionamento.realizar_extrato(extrato, saldo_total, saldo_diario)
+        funcionamento.realizar_extrato(extrato, saldo_total, saque_diario)
 
     elif operacao == 2:
-
         extrato, saldo_total = funcionamento.realizar_deposito(extrato, saldo_total)
 
-    elif operacao == 3 and saldo_diario > 0:
+    elif operacao == 3 and saque_diario > 0:
+        extrato, saldo_total, saque_diario = funcionamento.realizar_saque(extrato, saldo_total, saque_diario)
 
-        extrato, saldo_total, saldo_diario = funcionamento.realizar_saque(extrato, saldo_total, saldo_diario)
-
-    elif operacao == 3 and saldo_diario <= 0:
+    elif operacao == 3 and saque_diario <= 0:
         print("Saques diários excedidos!")
 
     elif operacao == 4:
